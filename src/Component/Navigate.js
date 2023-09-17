1901782a4b923ae06113b9f2fbb86bb5d973100b
Shriyansh7870 <Shriyansh7870@gmail.com>
2023-09-18 00:38:39 +0530

    Aone

--- src/Component/Navigate.js
+++ src/Component/Navigate.js
@@ -5,7 +5,7 @@ import { Store } from "../Context/ContextApi";
 const Navigate = () => {
   const [newitem] = useContext(Store);
   const { id } = useParams();
-  const itemId = parseInt(id, 10);
+  const itemId = parseInt(id);
   const selectedItem = newitem.find((item) => item.id === itemId);
   if (!selectedItem) {
     return <div>Item not found</div>;
